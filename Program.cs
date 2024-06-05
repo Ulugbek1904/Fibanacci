ca9336dc26fd4b75550a6840df8911a55a35918f
Ulugbek1904 <julugbek023@gmail.com>
2024-06-05 22:50:35 +0500

    Add Code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibanacci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.Write("Enter end point of fibanacci :");
+            int fibanacciEnd = int.Parse(Console.ReadLine());
+
+            int fibanacci1 = 0;
+            int fibanacci2 = 1;
+
+            while (fibanacci1 < fibanacciEnd)
+            {
+                System.Console.Write(fibanacci1 + " ");
+                int nextFibanacci = fibanacci1 + fibanacci2;
+                fibanacci1 = fibanacci2;
+                fibanacci2 = nextFibanacci;
+            }
+
+
+
+        }
+    }
+
+}
